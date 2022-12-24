42806423b8adfd9fb0551a3da718e0b7995a1723
Dan <dansonlinepresence@gmail.com>
2022-12-23 20:14:00 -0500

    stay on forever

--- stay-on.py
+++ stay-on.py
@@ -3,9 +3,11 @@
 import argparse
 import datetime
 import os
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('path', help='Where to put the stay-on-until.txt file.')
+parser.add_argument('--forever', '-f', action='store_true')
 args = parser.parse_args()
 
 if not args.path.endswith('stay-on-until.txt'):
@@ -13,7 +15,10 @@ if not args.path.endswith('stay-on-until.txt'):
 else:
     path = args.path
 
-hour_from_now = (datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(hours=1)).isoformat()
-with open(path, 'w') as f:
-    f.write(hour_from_now)
-print(f'staying on until {hour_from_now} (~1 hour)')
+while True:
+    hour_from_now = (datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(hours=1)).isoformat()
+    with open(path, 'w') as f:
+        f.write(hour_from_now)
+    print(f'staying on until {hour_from_now} (~1 hour)')
+    if not args.forever: break
+    time.sleep(30 * 60)
